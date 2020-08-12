c68b52268ee27532a5015cdcf87113e95ba28598
kensuke.saito <kensuke.saito@persol.co.jp>
2020-08-12 14:30:44 +0900

    add insertion sort.

---
+++ sorts/utils/initdata.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+import random
+
+def generate(num:int):
+  """ Generate random integer list
+
+  Args:
+      num (int): size of list
+
+  Returns:
+      Array: random integer list(size: num)
+  """
+  return [random.randint(0, num*100) for i in range(1, num)]
+
+if __name__ == '__main__':
+    generate(1)
